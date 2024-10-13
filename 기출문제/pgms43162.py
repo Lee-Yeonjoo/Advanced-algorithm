7dfe9708db3cc3853aa25dbbecc6b07e93952fd3
Yeonjoo Lee <joos0207@naver.com>
2024-10-14 00:48:09 +0900

    bfs 프로그래머스 풀이 수정

--- 기출문제/pgms43162.py
+++ 기출문제/pgms43162.py
@@ -1,34 +1,34 @@
 from collections import deque
 
-def bfs(n, computers, i, j):
+def bfs(n, computers, x, visited):
+    visited[x] = True  #시작 노드 방문처리
     queue = deque()
-    queue.append((i,j))
+    queue.append(x)  #큐에 시작노드 넣기
 
     while queue:
-        v = queue.popleft()
+        v = queue.popleft()  #큐에서 제일 앞의 노드 pop
 
-        if v[0] == v[1]:
-            computers[v[0]][v[1]] == 0  #자기 자신이라면 미리 방문처리
-
-        for j in range(0, n):
-            if computers[v[1]][j] == 1:
-                queue.append((v[1], j))
-                computers[v[1]][j] = 0  #방문 처리
+        for i in range(n):   #v의 이웃 노드에 대해 반복
+            if computers[v][i] == 1 and visited[i] == False:
+                queue.append(i)  
+                visited[i] = True  #이웃 노드 방문 처리
 
 def solution(n, computers):
     answer = 0
+    visited = [False] * n
     
-    for i in range(0, n):
-        for j in range(0, n):
-            if computers[i][j] == 1:
-                bfs(n, computers, i, j)
-                answer += 1
+    for i in range(n):
+        if visited[i] == False:
+            bfs(n, computers, i, visited)
+            answer += 1
 
     return answer
 
 n = 3
 computers = [
-    [1, 1, 0], [1, 1, 1], [0, 1, 1]
+    [1,1,0],
+    [1,1,1],
+    [0,1,1]
 ]
 
 print(solution(n, computers))
